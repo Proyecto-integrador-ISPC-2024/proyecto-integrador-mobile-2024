0b7148f6c92c256a04556822dc5c9d282882f6a9
valentachini <valentinatachini@gmail.com>
2024-10-08 20:11:14 -0300

    commit para actualizar cambios

--- app/src/main/java/com/example/tiendadecampeones/ui/Cart.java
+++ app/src/main/java/com/example/tiendadecampeones/ui/Cart.java
@@ -70,7 +70,6 @@ public class Cart extends AppCompatActivity {
         productList.add(new Product("Camiseta Argentina 2021", "Description 1", 19.99, R.mipmap.argentina_2021_primera, 1));
         productList.add(new Product("Camiseta Brasil 2002", "Description 2", 29.99, R.mipmap.brasil_2002_primera, 2));
         productList.add(new Product("Camiseta Alemania 2014", "Description 3", 39.99, R.mipmap.alemania_2014_segunda, 2));
-
         // Set adapter
         cartAdapter = new CartAdapter(productList, this);
         cartRecyclerView.setAdapter(cartAdapter);
