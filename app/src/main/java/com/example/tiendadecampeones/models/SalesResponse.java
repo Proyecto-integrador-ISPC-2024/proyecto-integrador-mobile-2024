8cb0b33bf4404bd927736c0d5b1e3e62d86847f0
Leopladom <114880850+Leopladom@users.noreply.github.com>
2025-06-03 23:37:02 -0300

    se implementa calculadora de ventas por periodos

---
+++ app/src/main/java/com/example/tiendadecampeones/models/SalesResponse.java
@@ -0,0 +1,13 @@
+package com.example.tiendadecampeones.models;
+
+public class SalesResponse {
+    private double total_ventas;
+
+    public double getTotalVentas() {
+        return total_ventas;
+    }
+
+    public void setTotalVentas(double total_ventas) {
+        this.total_ventas = total_ventas;
+    }
+}
